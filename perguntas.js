b2da10ba37ba28abf6fccfa9c17cd7da7d2a19df
Kingui09 <135466787+Kingui09@users.noreply.github.com>
2024-09-27 12:17:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'The Walking Dead',
+    ' Qual é o nome do personagem principal que acorda de um coma e descobre que o mundo foi tomado por zumbis?',
+    'Rick Grimes'
+)
+
+criaCartao(
+    'The Umbrella Academy',
+    'Qual é o nome do personagem que tem a habilidade de viajar no tempo?',
+    'Número Cinco'
+)
+
+criaCartao(
+    'Breaking Bad',
+    'Qual é o pseudônimo usado por Walter White no mundo do tráfico de drogas?',
+    'Heisenberg'
+)
+
+criaCartao(
+    'Stranger Things',
+    'Qual é o nome da cidade fictícia onde se passa a série Stranger Things?',
+    'Hawkins'
+)
